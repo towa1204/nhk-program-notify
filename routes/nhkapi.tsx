91a120c60dcd0d1f774e2f087c8186f0a511ba8d
towa1204 <lsadsfj@gmail.com>
2025-01-11 21:08:31 +0900

    refactor: simplify error message

--- routes/nhkapi.tsx
+++ routes/nhkapi.tsx
@@ -123,7 +123,7 @@ export default function NhkApiPage(
         </hgroup>
       </header>
 
-      {errorMessage != undefined && (
+      {errorMessage && (
         <div
           class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 whitespace-pre-wrap"
           role="alert"
--- routes/program.tsx
+++ routes/program.tsx
@@ -61,7 +61,7 @@ export default function ProgramPage(
         </hgroup>
       </header>
 
-      {errorMessage != undefined && (
+      {errorMessage && (
         <div
           class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
           role="alert"
